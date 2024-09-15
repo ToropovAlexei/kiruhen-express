3106b72eb73281808c3712432ee000c6542932d8
ToropovAlexey <henschel37rus@gmail.com>
2024-09-15 20:04:14 +0300

    added admin page

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import express from "express";
 import { sneakerRoutes } from "./routes/sneakerRoutes";
 import { cartRoutes } from "./routes/cartRoutes";
 import { favoritesRoutes } from "./routes/favoritesRoutes";
+import cors from "cors";
 
 export const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 
 app.use("/api", sneakerRoutes);
 app.use("/api", cartRoutes);
